57ba00e02ace2d7e9024561cf070263c57b7aa9d
Shao Ge <geshao3606@gmail.com>
2021-06-12 14:59:39 -0700

    testing results

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
 const FILES_TO_CACHE = [
-    '/db.js',
     '/index.html',
     '/index.js',
     '/styles.css',
+    '/indexedDb.js',
     '/manifest.webmanifest',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
