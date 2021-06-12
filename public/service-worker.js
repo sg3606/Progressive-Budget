8ef2932192ace3819603c1e7f76e6f63616883bb
Shao Ge <geshao3606@gmail.com>
2021-06-12 11:57:22 -0700

    test manifest and service-work

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,41 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
+    "/",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ]
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
+
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
