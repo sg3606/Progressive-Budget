0e4a2b10dcdbba86e299541fc09077f6715b0465
Shao Ge <geshao3606@gmail.com>
2021-06-12 12:07:25 -0700

    service-worker fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    '/',
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
 ]
 
 const PRECACHE = 'precache-v1';
@@ -19,6 +20,24 @@ self.addEventListener('install', (event) => {
     );
 });
 
+self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
 
 self.addEventListener('fetch', (event) => {
     if (event.request.url.startsWith(self.location.origin)) {
